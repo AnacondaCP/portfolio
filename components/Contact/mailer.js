0aaf09f77c17411384547064084e930461c9f070
Bryanine.899 <103065321+AnacondaCP@users.noreply.github.com>
2024-08-29 22:23:59 +0700

    Update mailer.js

--- components/Contact/mailer.js
+++ components/Contact/mailer.js
@@ -2,15 +2,15 @@ import emailjs from "@emailjs/browser";
 
 const mail = ({ name, email, message }) =>
   emailjs.send(
-    process.env.NEXT_PUBLIC_SERVICE_ID,
-    process.env.NEXT_PUBLIC_TEMPLATE_ID,
-    { name, email, message },
+    process.env.NEXT_PUBLIC_SERVICE_ID, // Ambil dari variabel lingkungan
+    process.env.NEXT_PUBLIC_TEMPLATE_ID, // Ambil dari variabel lingkungan
     {
-      publicKey: process.env.NEXT_PUBLIC_USER_ID,
-      limitRate: {
-        throttle: 10000, // 10s
-      },
-    }
+      to_name: "Recipient Name", // Ganti dengan nama penerima jika diperlukan
+      from_name: name, // Nama pengirim dari form
+      from_email: email, // Email pengirim dari form
+      message: message, // Pesan dari format
+    },
+    process.env.NEXT_PUBLIC_USER_ID // Ambil dari variabel lingkungan
   );
 
 export default mail;
